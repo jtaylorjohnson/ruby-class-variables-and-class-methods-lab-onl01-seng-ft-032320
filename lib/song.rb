79d37138fc849035af5b681575e0af184cf28305
James Johnson <jtaylorjohnson@gmail.com>
2020-04-05 06:15:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,18 +27,22 @@ class Song
   
   def self.genre_count
     
-    @genre_hash = {}
+    genre_hash = {}
     @@genres.each do |genre|
-      if @genre_hash.include?(genre) == false 
-        @genre_hash[genre] = [1]
-      else current = @genre_hash[genre]
-        @genre_hash[genre] = current + 1 
+      if genre_hash.include?(genre) == false 
+        genre_hash[genre] = 1
+      else genre_hash[genre] += 1 
       end
     end
   end
   
   def self.artist_count
-    
+    artist_hash = {}
+    @@genres.each do |genre|
+      if artist_hash.include?(genre) == false 
+        genre_hash[genre] = 1
+      else genre_hash[genre] += 1 
+      end
   end
   
 end
