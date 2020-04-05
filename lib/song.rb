201558c98de90f84dba70d278f04e12c4996960e
James Johnson <jtaylorjohnson@gmail.com>
2020-04-05 04:46:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 class Song 
   attr_accessor :name :artist :genre 
-  @@count = 0 
+  @@count = 0
+  @@artists = []
+  @@genres = []
   
   def initialize 
     @@count += 1
@@ -8,6 +10,14 @@ class Song
   
   def self.count
     @@count 
+  end
+  
+  def self.artists
+    
+  end
+    
+  def self.genres 
+    
   end
   
   def genre_count
